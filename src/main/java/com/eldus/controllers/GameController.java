ac40d44f480773ca48a7ba6383f92ea959fdd747
Eldus <arsik2001@gmail.com>
2021-04-10 15:18:55 +0300

    refactor commit

--- src/main/java/com/eldus/controllers/GameController.java
+++ src/main/java/com/eldus/controllers/GameController.java
@@ -1,51 +1,53 @@
 package com.eldus.controllers;
 
-import com.eldus.CityService;
+import com.eldus.CityServiceDbImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Controller
 public class GameController {
-    private final CityService cityService;
+    private final CityServiceDbImpl cityServiceDbImpl;
     private Map<String, String> gameSessions;
-    public GameController(CityService cityService) {
-        this.cityService = cityService;
+
+    public GameController(CityServiceDbImpl cityServiceDbImpl) {
+        this.cityServiceDbImpl = cityServiceDbImpl;
         gameSessions = new HashMap<>();
     }
 
 
-
-    @RequestMapping(value = "/game", method = RequestMethod.GET,produces = "text/plain;charset=UTF-8")
-    public String printGame(ModelMap model,@RequestParam(required = true) String id) {
-        gameSessions.put(id,null);
-        cityService.newGameTable(id.replace("-","_"));
+    @RequestMapping(value = "/game", method = RequestMethod.GET, produces = "text/plain;charset=UTF-8")
+    public String printGame(ModelMap model, @RequestParam String id) {
+        gameSessions.put(id, null);
+        cityServiceDbImpl.newGameTable(id.replace("-", "_"));
 
         return "hello";
     }
 
     @PostMapping("/game")
-    public String check(ModelMap model,@RequestParam String fname, @RequestParam(required = true) String id) {
-        if(gameSessions.get(id)==null || cityService.lastLetterCheck(gameSessions.get(id))==fname.charAt(0)){
-            String s = cityService.findRandomCity(fname,id.replace("-","_"));
-            if(s.equals("Такого города не существует!")){
-                model.addAttribute("message", s +" " + gameSessions.get(id) );
+    public String check(ModelMap model, @RequestParam String fname, @RequestParam String id) {
+        if (gameSessions.get(id) == null || cityServiceDbImpl.lastLetterCheck(gameSessions.get(id)) == fname.charAt(0)) {
+            String s = cityServiceDbImpl.findRandomCity(fname, id.replace("-", "_"));
+            if (s.equals("Такого города не существует!")) {
+                model.addAttribute("message", s + " " + gameSessions.get(id));
                 return "hello";
             }
-            if(s.equals("Сдаюсь")){
+            if (s.equals("Сдаюсь")) {
                 return "win";
             }
-            gameSessions.put(id,s);
-            model.addAttribute("message",s);
-        System.out.println(model.getAttribute("message"));
+            gameSessions.put(id, s);
+            model.addAttribute("message", s);
+            System.out.println(model.getAttribute("message"));
 
-        return "hello";
-        }
-        else {
-            model.addAttribute("message", "Неправильный город " + gameSessions.get(id) );
+            return "hello";
+        } else {
+            model.addAttribute("message", "Неправильный город " + gameSessions.get(id));
             return "hello";
         }
     }
